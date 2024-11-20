37ef9513ed0d503832d5ae74d629793816024096
Goitom <goitommahtsentu@gmail.com>
2024-11-20 21:44:15 +0100

    adding google auth

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express"
-import {signIn, signUp} from "../controller/auth.controller.js";
+import {google, signIn, signUp} from "../controller/auth.controller.js";
 
 const router = express.Router()
 
 router.post('/sign-in',signIn )
 router.post('/sign-up',signUp )
-router.post('/sign-out', )
+router.post('/google',google)
 
 export default router
