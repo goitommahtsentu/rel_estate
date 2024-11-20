7bfe4039c61b0ef009b5acfd8e0a1512feaa77bc
Goitom <goitommahtsentu@gmail.com>
2024-11-20 21:54:24 +0100

    Add client-side code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {FaSearch} from "react-icons/fa";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const Header = () => {
+    const {currentUser}=useSelector(state => state.user)
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -18,7 +20,6 @@ const Header = () => {
                     <FaSearch className='text-slate-600'/>
                 </form>
                 <ul className='flex gap-4'>
-                    <>
                         <Link to='/'>
                             <li className='hidden sm:inline text-slate-700 hover:underline '>Home</li>
                         </Link>
@@ -26,12 +27,13 @@ const Header = () => {
                         <Link to='/about'>
                             <li className='hidden sm:inline text-slate-700 hover:underline '>about</li>
                         </Link>
-
-                        <Link to='/sign-in'>
-                            <li className='hidden sm:inline text-slate-700 hover:underline '>Sign in</li>
+                        <Link to='/pofile'>
+                        {currentUser ? (<img className='rounded-full w-7 h-7' src={currentUser.avatar} alt="profile"/>):
+                            ( <Link to='/sign-in'>
+                                <li className='hidden sm:inline text-slate-700 hover:underline '>Sign in</li>
+                            </Link>)
+                        }
                         </Link>
-
-                    </>
 
                 </ul>
             </div>
